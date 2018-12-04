abb494e33d4db42eae5d5d7adf68906517787ce3
zyktrcn <594823346@qq.com>
2018-12-04 18:13:20 +0800

    clear

--- tuna-app/registerAdmin.js
+++ tuna-app/registerAdmin.js
@@ -1,13 +1,4 @@
 'use strict';
-/*
-* SPDX-License-Identifier: Apache-2.0
-*/
-/*
- * Chaincode Invoke
-
-This code is based on code written by the Hyperledger Fabric community.
-  Original code can be found here: https://gerrit.hyperledger.org/r/#/c/14395/4/fabcar/enrollAdmin.js
- */
 
 var Fabric_Client = require('fabric-client');
 var Fabric_CA_Client = require('fabric-ca-client');
@@ -24,14 +15,12 @@ var member_user = null;
 var store_path = path.join(os.homedir(), '.hfc-key-store');
 console.log(' Store path:'+store_path);
 
-// create the key value store as defined in the fabric-client/config/default.json 'key-value-store' setting
 Fabric_Client.newDefaultKeyValueStore({ path: store_path
 }).then((state_store) => {
-    // assign the store to the fabric client
+
     fabric_client.setStateStore(state_store);
     var crypto_suite = Fabric_Client.newCryptoSuite();
-    // use the same location for the state store (where the users' certificate are kept)
-    // and the crypto store (where the users' keys are kept)
+
     var crypto_store = Fabric_Client.newCryptoKeyStore({path: store_path});
     crypto_suite.setCryptoKeyStore(crypto_store);
     fabric_client.setCryptoSuite(crypto_suite);
@@ -39,10 +28,10 @@ Fabric_Client.newDefaultKeyValueStore({ path: store_path
     	trustedRoots: [],
     	verify: false
     };
-    // be sure to change the http to https when the CA is running TLS enabled
+
     fabric_ca_client = new Fabric_CA_Client('http://localhost:7054', tlsOptions , 'ca.example.com', crypto_suite);
 
-    // first check to see if the admin is already enrolled
+
     return fabric_client.getUserContext('admin', true);
 }).then((user_from_store) => {
     if (user_from_store && user_from_store.isEnrolled()) {
@@ -50,7 +39,7 @@ Fabric_Client.newDefaultKeyValueStore({ path: store_path
         admin_user = user_from_store;
         return null;
     } else {
-        // need to enroll it with CA server
+      
         return fabric_ca_client.enroll({
           enrollmentID: 'admin',
           enrollmentSecret: 'adminpw'
